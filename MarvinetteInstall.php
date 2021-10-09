0cc3ab06f6f8276edcbf005291ecb5dc2cf392dc
arthur.jamet <arthur.jamet@epitech.eu>
2021-10-09 14:49:12 +0200

    linux installation: move folder to .local/bin

--- MarvinetteInstall.php
+++ MarvinetteInstall.php
@@ -2,21 +2,38 @@
 
 $isWindows = sys_get_temp_dir() != '/tmp';
 
+function copyFolder(string $sourcePath, string $destPath)
+{
+	if (!is_dir($destPath))
+		mkdir($destPath);
+	foreach (glob("$sourcePath/*") as $file) {
+		if (is_dir($file))
+			copyFolder($file, "$destPath/" . basename($file));
+		else
+			copy($file, "$destPath/" . basename($file));
+	}
+}
+
 function install()
 {
 	global $isWindows;
+	var_dump($_SERVER);
 	$CWD = getcwd();
+	$scriptName = 'marvinette';
+
 	if (!$CWD)
 		throw new Exception('Impossible to get current working directory');
-	$scriptName = 'marvinette';
-	if (!$isWindows) {
-		$scriptPath = "/usr/local/bin/";
-	} else {
+	if ($isWindows) {
 		$scriptPath = '.' . DIRECTORY_SEPARATOR;
 		$scriptName .= '.ps1';
+	} else {
+		$scriptPath = $_SERVER['HOME'] . "/.local/bin/";
 	}
 	file_put_contents($scriptPath . $scriptName, getScriptContent($CWD, $isWindows));
 	chmod($scriptPath . $scriptName, 0777);
+	if (!$isWindows) {
+		copyFolder(".", $_SERVER['HOME'] . "/.local/lib/marvinette");
+	}
 }
 
 function getScriptContent(string $projectPath, bool $isWindows = false): string
@@ -26,7 +43,7 @@ function getScriptContent(string $projectPath, bool $isWindows = false): string
 	if (!$isWindows) {
 		$content = [
 			"#!/bin/sh",
-			'php  ' . '~/.local/lib/marvinette/src/main.php $@',
+			'php ~/.local/lib/marvinette/src/main.php $@',
 			'exit $?'
 		];
 	} else {
